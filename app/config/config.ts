5ee70dc9a6504ef693cefd79d9a4f189b456fab2
Manuel Cappai <manuelcappai@outlook.it>
2026-04-11 22:30:37 +0200

    minor-change

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production"
 
-export const server = dev? "http://localhost:3000" : `https://${process.env.VERCEL_URL}`
+export const server = dev 
+? "http://localhost:3000" 
+: "https://web-dev-next-js-git-main-manuelcappai94s-projects.vercel.app"
